cd08eed95739d70b2670320917eb45c7a9c580f5
Gerard <gerardbriangueco@gmail.com>
2025-01-17 02:23:11 +0800

    remove dark mode coded

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,7 +1,5 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { DarkModeService } from 'angular-dark-mode';
-import { Observable } from 'rxjs';
 
 @Component({
   imports: [CommonModule],
@@ -10,15 +8,7 @@ import { Observable } from 'rxjs';
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-  readonly darkMode$: Observable<boolean>;
-
-  constructor(private readonly darkModeService: DarkModeService) {
-    this.darkMode$ = this.darkModeService.darkMode$;
-  }
+  constructor() {}
 
   ngOnInit(): void {}
-
-  onToggle(): void {
-    this.darkModeService.toggle();
-  }
 }
